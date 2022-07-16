19f8f5b738d09f273c511932c4035dd24d58e1e0
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-07-16 09:38:46 -0500

    Able to set character and name

--- js/app.js
+++ js/app.js
@@ -1,6 +1,46 @@
+//Character Class
 class Character {
     constructor(name) {
     this.name = name
     this.skills = []
     }
 }
+
+//Consts
+const introClass = document.querySelectorAll(`.intro`)
+const skillsClass = document.querySelectorAll(`.skills`)
+const battleClass = document.querySelectorAll(`.battle`)
+
+const nameSelect = document.querySelector(`#nameInput`)
+const submitName = document.querySelector(`#nameSubmit`)
+
+const strStat = document.querySelector(`#strStat`)
+const strDown = document.querySelector(`#strDwn`)
+const strUp = document.querySelector(`#strUp`)
+
+const intStat = document.querySelector(`#intStat`)
+const intDwn = document.querySelector(`#intDwn`)
+const intUp = document.querySelector(`#intUp`)
+
+const dexStat = document.querySelector(`#dexStat`)
+const dexDwn = document.querySelector(`#dexDwn`)
+const dexUp = document.querySelector(`#dexUp`)
+
+const stats = document.querySelector(`.stats`)
+
+//create character
+const character = new Character(``)
+
+//Submit name and go to skills
+const intro = ()  =>  {
+    if(nameSelect.value === ``) {
+        alert(`Please choose a name!`)
+    } else {
+    character.name = nameSelect.value
+    console.log(character)
+    }
+    // getElementsByClassName(`intro`).style.display = `none`
+    // skillsClass.style.display = `block`
+}
+
+submitName.addEventListener(`click`, intro)
